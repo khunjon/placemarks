6926ea4a77c1ebc5a8e470c772f4f78971bd42c5
khunjon <jonathan.thongtan@gmail.com>
2025-06-16 11:40:28 +0700

    nativewind + react native elements installation

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { AuthProvider } from './src/services/auth-context';
 import Navigation from './src/components/Navigation';
+import './global.css';
 
 export default function App() {
   return (
